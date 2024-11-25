a2378a13bb9e7fcc48bbf39ab6b1a1187aa29bbe
Tycho <25345689+Tyfcho@users.noreply.github.com>
2024-11-25 14:03:01 +0100

    Added a tad bit more context to the TCASCommand.java class.

--- src/main/java/me/tyfcho/tcas/commands/TCASCommand.java
+++ src/main/java/me/tyfcho/tcas/commands/TCASCommand.java
@@ -9,7 +9,16 @@ import me.tyfcho.tcas.attractions.AttractionMode;
 import org.bukkit.entity.Player;
 
 /**
- * Overview of the Commands System The main command for your plugin will be /tcas, and it will have several subcommands to handle different aspects of attraction management. These subcommands will include: /tcas create: Opens the GUI for creating a new attraction. /tcas dispatch: Starts the selected attraction. /tcas mode  auto/manual/maintenance>: Switches between different operating modes. /tcas power: Claims control over the attraction (for operator mode). /tcas showtrigger: Displays the current status of the attraction’s trigger. /tcas gates: Controls the gates of the attraction (open/close). /tcas restraints: Controls the restraints (for coasters or other rides).
+ * Overview of the Commands System.
+ * The main command for the plugin will be /tcas, and it will have several subcommands to handle different aspects of attraction management.
+ * These subcommands will include:
+ * - /tcas create: Opens the GUI for creating a new attraction.
+ * - /tcas dispatch: Starts the selected attraction (has prerequisites from gates, restraints, traininstation, if applicable).
+ * - /tcas mode  auto/manual/maintenance>: Switches between different operating modes (administrator only for maintenance). 
+ * - /tcas power: Claims control over the attraction (for operator mode, together with initializing the attraction).
+ * - /tcas showtrigger: Displays the current status of the attraction’s trigger (e.g. animation of a floor with a dive coaster).
+ * - /tcas gates: Controls the gates of the attraction (only applicable if configured, open/close).
+ * - /tcas restraints: Controls the restraints (only applicable if configured, same logic as gates).
  */
 public class TCASCommand {
 
@@ -54,10 +63,43 @@ public class TCASCommand {
         // Additional logic here...
     }
 
+    /**
+     * The dispatch command can only be ran by the user that has "claimed" the attraction.
+     * The dispatch command will only be ran when the following pre-requisites are met:
+     * - Attraction is in station / ride sequence has been completed (default value = 0)
+     * - Gates are closed (if applicable)
+     * - Restraints are closed (if applicable)
+     * - Command was called by operating user
+     * 
+     * Executing the command will be as follows:
+     * - /tcas dispatch <attraction>
+     * 
+     * Optional for administrators: 
+     * - force: /tcas dispatch <attraction> -f
+     * - debug: /tcas dispatch <attraction> -d
+     * 
+     * Forcing the dispatch:
+     * - Will skip the pre-requisites
+     * 
+     * Debugging the dispatch:
+     * - Will go through every step of the sequence
+     * - Will print the print-out to the executor
+     */
     private void handleDispatchCommand(CommandContext<Player> context) {
         Player player = context.getSender();
+        Attraction attraction = plugin.getAttractionManager().getAttractionForPlayer(player);
+        String admin = context.get("admin");
+
+        if (admin != null) {
+            //TODO: Add logic to here to check whether player has "tcas.admin" permission node
+            /**
+             * Forcing the dispatch will run the complete "dispatch sequence".
+             * Forcing the dispatch will always tell user what outcome of run sequence is
+             * 
+             */
+        }
+        // Add logic here
         player.sendMessage("Dispatching the attraction...");
-        // Additional logic here...
     }
 
     private void handleModeCommand(CommandContext<Player> context) {
